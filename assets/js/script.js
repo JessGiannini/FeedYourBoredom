77f33e0735cd73a975f9c8dfdc5f27701b0754f4
qtian13 <qiushuang.tian@gmail.com>
2021-07-07 18:40:23 -0700

    display bored api result with user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,58 @@
-var modal = document.getElementById("user-modal");
+var modal = document.getElementById("user-modal-1");
 var btn = document.getElementById("search-btn");
 var span = document.getElementsByClassName("close")[0];
 var modalHeader = $("h3").text("Please, fill out this form.");
-var activityType = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"];
-var participants;
-var typeSelected;
+var activityType = ["random", "education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"];
+// key: number of participants; value: all possible types of results
+// var activityType = {
+//   1: ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"],
+//   2: ["social"],
+//   3: ["social"],
+//   4: ["social", "music", "recreational"],
+//   5: ["social", "music"]
+// }
+
 
 
 // function to deal with bored api
-function getActivity() {
-  participants = $("#participants-input").val();
-  typeSelected = $("#activity-type-select").val();
-  var requestURL = "http://www.boredapi.com/api/activity/?" + "participants=" + participants + "type=" + typeSelected; 
+function submitEventHandler() {
+  event.preventDefault();
+  var participants = $("#participants-input").val();
+  var participantsQueryParameter = participants == "" ? ""
+                                        : "participants=" + participants + "&";
+  var typeSelected = $("#activity-type-select").val();
+
+  // display the user input
+  var participantsEl = $("<div>").text("participants: " + participants);
+  var typeEl = $("<div>").text("Type: " + typeSelected);
+  $(".user-input-record").html("");
+  $(".user-input-record").append(participantsEl, typeEl);
+  var requestURL = "http://www.boredapi.com/api/activity/?" + participantsQueryParameter + "type=" + typeSelected; 
   fetch(requestURL)
   .then(function (res) {
     return res.json();
   })
   .then(function (data) {
-    console.log("activity");
+    if (data.error != undefined) {
+      $(".search-result-display").append("No results found");
+      return;
+    }
+    //there is one more query parameter key: unique and can be used to search a certain activity
+    var activityEl = $("<h3>").text("activity: " + data.activity);
+    // var participantsEl = $("<div>").text("participants: " + data.participants);
+    var priceEl = $("<div>").text("price: " + data.price);
+    // var typeEl = $("<div>").text("type: " + data.type);
+    var accessibilityEl = $("<div>").text("accessibility: " + data.accessibility);
+    // price and accessibility can be displayed using empty or colored star
+    $(".search-result-display").html("");
+    $(".search-result-display").append(activityEl, priceEl, accessibilityEl);
     console.log(data);
   });
-
 }
 
-
+for(var i = 0; i < activityType.length; i++) {
+  $("#activity-type-select").append($("<option>").text(activityType[i]));
+}
 
 fetch(
   "https://cors.bridged.cc/https://api.yelp.com/v3/businesses/search?location=san%20jose",
@@ -46,13 +75,6 @@ fetch(
   });
 
 
-
-for(var i = 0; i < activityType.length; i++) {
-  $("#activity-type-select").append($("<option>").text(activityType[i]));
-}
-
-
-
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
   modal.style.display = "block";
@@ -69,3 +91,5 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+$(document).on("click", "#submit-button", submitEventHandler)
